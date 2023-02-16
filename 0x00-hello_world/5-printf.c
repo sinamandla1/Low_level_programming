62fd078f2390c5d6382148232b16af807918e38f
sinamandla1 <sinamandlableu@gmail.com>
2023-02-16 12:18:13 -0800

    fix number 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'a C program that prints a line using the printf function'.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
