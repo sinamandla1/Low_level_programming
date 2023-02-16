03581bafd3e1a0121cc018eba50e2d9cae40294b
sinamandla1 <sinamandlableu@gmail.com>
2023-02-16 07:30:10 -0800

    new files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - a C program that prints out a line using printf function
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
