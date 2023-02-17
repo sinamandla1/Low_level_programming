e7421682893b92faaf1993fe1a11da0276edcb38
sinamandla1 <sinamandlableu@gmail.com>
2023-02-16 17:18:34 -0800

    all files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unlistd.h>
+/**
+ * main - Entry
+ * Description: 'Program that prints a string without using printf and puts'
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(5, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
