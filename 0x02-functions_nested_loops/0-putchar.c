5605a4c57d38ddfb791d2e0db3f6dcdf2ce3142a
sinamandla1 <sinamandlableu@gmail.com>
2023-02-21 03:34:28 -0800

    new directory 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: a program that prints _putchar, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
