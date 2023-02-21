763c2ed5ef71d25538c2229caee148746f97d704
sinamandla1 <sinamandlableu@gmail.com>
2023-02-21 09:11:37 -0800

    fix file 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
