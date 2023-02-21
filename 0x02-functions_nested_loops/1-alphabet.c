8ff7cdacf935096b601a957221f01c3581f06af6
sinamandla1 <sinamandlableu@gmail.com>
2023-02-21 11:27:53 -0800

    new file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_alphabet - entry point
+ * description: a function to print out the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
