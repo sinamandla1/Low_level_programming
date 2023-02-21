9635820fc9644f4c9eec9497a4d88858cd1ee7ae
sinamandla1 <sinamandlableu@gmail.com>
2023-02-21 11:48:08 -0800

    new files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - entry point
+ * description: prints out the alphabet ten times
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int i = 10;
+
+	while (i = 10)
+	{
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
