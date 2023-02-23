69be950c12cf012b3fcb6b563d8f58cabc36d792
sinamandla1 <sinamandlableu@gmail.com>
2023-02-23 15:47:30 -0800

    new directory

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _isupper - Check if a letter is upper
- * @x: The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
-int _isupper(int x)
+int _isupper(int c)
 {
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _isdigit - Check if a character is a digit
- * @x: The number to be checked
- * Return: 1 for a character that will be a digit or 0 for any else
+ * _isdigit - Checks for a digit 0 to 9
+ * @c:  the digit to be checked is c
+ *
+ * Return: 1 if true, 0 otherwise.
  */
-int _isdigit(int x)
+int _isdigit(int c)
 {
-
-	if (x >= 48 && x <= 57)
-	{
-	return (1);
-	}
-	return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
