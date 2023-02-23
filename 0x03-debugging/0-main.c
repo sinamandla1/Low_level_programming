4f4bbcbee3682c8ee3459e334c4f552019cfd959
sinamandla1 <sinamandlableu@gmail.com>
2023-02-23 05:46:30 -0800

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: integer
-*/
-void positive_or_negative(int i)
-{
-        int i;
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
-        i = 0;
+int main(void)
+{
+	int i;
+
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
