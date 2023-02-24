faeb0f22fe4bbe9fbe820767ca1c885633bda9ac
sinamandla1 <sinamandlableu@gmail.com>
2023-02-23 18:43:41 -0800

    new file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main..h"
+/**
+ * print_numbers - entry point
+ * description -a function that print numbers 0 to 9
+ * Return: numbers 0 to 9
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a = < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
