82afe369eb4f203fe899de09c558d28af398a57a
sinamandla1 <sinamandlableu@gmail.com>
2023-02-23 18:49:08 -0800

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,10 @@
  * Return: numbers 0 to 9
  */
 void print_numbers(void)
-
 {
+	int num;
 
-	int x;
-
-	for (x = 0; x <= 9; x++)
-	{
-	_putchar(x + '0');
-	}
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
 	_putchar('\n');
 }
