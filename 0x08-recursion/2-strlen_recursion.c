15f96f8f8ff9bfeb381baf6b18471a631bbbc1b3
sinamandla1 <sinamandlableu@gmail.com>
2023-03-08 19:38:52 -0800

    new files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        return (0);
-    }
-    s++;
-    return (1 + _strlen_recursion(s));
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y.
+ * _pow_recursion - returns the value of x raised to the power of y.
  * @x: integer
  * @y: integer2
  * Return: integer
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return (-1);
-    }
-    if (y == 0)
-    {
-        return (1);
-    }
-    return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
