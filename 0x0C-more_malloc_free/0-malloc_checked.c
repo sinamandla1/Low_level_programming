9e92556f4214ce894a2c7c351a506dca1a70ae9e
sinamandla1 <sinamandlableu@gmail.com>
2023-03-16 11:39:58 -0700

    new directory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memorry using malloc.
+ * @b: bytes to be allocated.
+ * Return: a pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
