af964c9077cb5373b072efbddf666852ec7dac0a
sinamandla1 <sinamandlableu@gmail.com>
2023-03-22 15:20:31 -0700

    hot fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-		{
 			return (i);
-		}
-		if (size <= 0 || cmp == NULL || array == NULL)
-		{
-			return (-1);
-		}
 	}
 	return (-1);
 }
