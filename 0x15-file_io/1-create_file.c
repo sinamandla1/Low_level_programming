58c1f445f842062acca443a752e1b72d593ee39d
sinamandla1 <sinamandlableu@gmail.com>
2023-04-12 18:18:55 -0700

    hot fix betty error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * create_file - function that creates a file.
+ * create_file - function that creates a file
  * @filename: pointer to file name
  * @text_content: pointer to string to put on the file
- * Returns: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
