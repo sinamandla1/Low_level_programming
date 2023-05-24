54b7ade818a94c0715232ee5f39157d66cba9491
sinamandla1 <sinamandlableu@gmail.com>
2023-05-24 06:04:38 -0700

    fixed malloc issue

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	newNode->n = n;
 	newNode->next = NULL;
-
 	if (newNode == NULL)
 	{
 		return (NULL);
