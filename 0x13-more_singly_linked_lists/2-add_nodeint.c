29e0996a578eb6e019c7ea7242003eae211948b1
sinamandla1 <sinamandlableu@gmail.com>
2023-05-24 06:02:03 -0700

    fixed betty issues

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,19 +2,18 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @n: dbl ptr to node at the beginning of the list
+ * @head: prt to first node
  * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *nextNode;
-	nextNode = malloc(sizeof(listint_t));
 
+	nextNode = malloc(sizeof(listint_t));
 	if (nextNode == NULL)
 		return (NULL);
-
 	nextNode->n = n;
 	nextNode->next = *head;
 	*head = nextNode;
-
 	return (nextNode);
 }
